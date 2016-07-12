52d0e985e718012797d18b78c4cba91718e1326c
Erick Almeida <ephillipe@gmail.com>
2016-07-12 08:04:35 -0300

    Update nfs_setup.sh

--- nfs_setup.sh
+++ nfs_setup.sh
@@ -10,7 +10,7 @@ for mnt in "${mounts[@]}"; do
   src=$(echo $mnt | awk -F':' '{ print $1 }')
   mkdir -p $src
   echo "$src $HOSTS$PARAMS" >> /etc/exports
-  echo "Added: $src $PARAMS"		 +  echo "Added: $src $HOSTS$PARAMS"
+  echo "Added: $src $HOSTS$PARAMS"
 done
 
 exec runsvdir /etc/sv
