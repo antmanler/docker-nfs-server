e5d35e3dcee4c015c20d094e7a4597b50bc6be89
Erick Almeida <ephillipe@gmail.com>
2016-07-12 08:47:15 -0300

    unable to resolve ANYADDR:nfs to inet address
    rpc.nfsd: unable to resolve ANYADDR:nfs to inet address: Servname not supported for ai_socktype
    rpc.nfsd: unable to resolve ANYADDR:nfs to inet6 address: Servname not supported for ai_socktype
    rpc.nfsd: unable to set any sockets for nfsd

--- nfs_setup.sh
+++ nfs_setup.sh
@@ -7,6 +7,17 @@ HOSTS="${HOSTS:-*}"
 PARAMS="${PARAMS:-(rw,sync,no_root_squash,no_subtree_check,insecure,fsid=0)}"
 
 modprobe nfs
+# NFS default ports
+echo "nfs        2049/tcp" >> /etc/services 
+echo "nfs        2049/udp" >> /etc/services
+echo "nfs        111/tcp" >> /etc/services 
+echo "nfs        111/udp" >> /etc/services
+# Ports to NFS Cluster and Client status
+echo "nfs        1110/tcp" >> /etc/services 
+echo "nfs        1110/udp" >> /etc/services
+# Ports to NFS lock manager
+echo "nfs        4045/tcp" >> /etc/services 
+echo "nfs        4045/udp" >> /etc/services
 
 for mnt in "${mounts[@]}"; do
   src=$(echo $mnt | awk -F':' '{ print $1 }')
