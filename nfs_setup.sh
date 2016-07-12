46150708008c56f23a95871d0ef97dc897c7cc13
Erick Almeida <ephillipe@gmail.com>
2016-07-12 08:51:42 -0300

    Update nfs_setup.sh

--- nfs_setup.sh
+++ nfs_setup.sh
@@ -7,17 +7,10 @@ HOSTS="${HOSTS:-*}"
 PARAMS="${PARAMS:-(rw,sync,no_root_squash,no_subtree_check,insecure,fsid=0)}"
 
 modprobe nfs
-# NFS default ports
-echo "nfs        2049/tcp" >> /etc/services 
-echo "nfs        2049/udp" >> /etc/services
-echo "nfs        111/tcp" >> /etc/services 
-echo "nfs        111/udp" >> /etc/services
-# Ports to NFS Cluster and Client status
-echo "nfs        1110/tcp" >> /etc/services 
-echo "nfs        1110/udp" >> /etc/services
-# Ports to NFS lock manager
-echo "nfs        4045/tcp" >> /etc/services 
-echo "nfs        4045/udp" >> /etc/services
+echo "nfs		     2049/tcp		# Network File System" >> /etc/services 
+echo "nfs        2049/udp   # Network File System" >> /etc/services
+echo "sunrpc		 111/tcp		portmapper	# RPC 4.0 portmapper" >> /etc/services 
+echo "sunrpc     111/udp    portmapper" >> /etc/services
 
 for mnt in "${mounts[@]}"; do
   src=$(echo $mnt | awk -F':' '{ print $1 }')
